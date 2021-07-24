fe094a9cb413dbb368aa0736b133eb18430aa37c
Harish064 <87409679+Harish064@users.noreply.github.com>
2021-07-24 19:55:12 +0530

    Modularised the code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Home from './components/Home/Home';
 function App() {
   return (
     <>
-      <Header/>
+      <Header height="755px">
+            <p className={styles.text}>All-IN-ONE SOLUTION PROVIDER FOR YOUR TECHNOLOGICAL NEEDS SYSTEM LEVEL SOLUTIONS</p>
+            <hr className={styles.line}></hr>
+            <p className={styles.sub_text}>Industry leaders in providing solutions catering to the domains of intellectual property, hardware design & prototyping, software design, and manufacturing.</p>
+      </Header>
       <Home />
     </>
   );
