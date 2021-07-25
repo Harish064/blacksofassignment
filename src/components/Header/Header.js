b5a7406e43b169cec61d8bd4a1954c9de0de3939
Harish064 <87409679+Harish064@users.noreply.github.com>
2021-07-25 10:38:01 +0530

    Commited after doing vision

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styles from './Header.module.css'
 function Header(props) {
+    const header=styles[props.header]
+    const image=styles[props.image]
+    const layer = styles[props.layer]
     const {height} = props
     return (
-        <div style={{height:{height}}} className={styles.header}>
-            <div style={{height:{height}}} className={styles.image}>
-                <div style={{height:{height}}} className={styles.layer}>
+        <div style={{height:{height}}} className={header}>
+            <div style={{height:{height}}} className={image}>
+                <div style={{height:{height}}} className={layer}>
                 </div>
             </div>
             {props.children}
         </div>
     )
 }
-
+ 
 export default Header
